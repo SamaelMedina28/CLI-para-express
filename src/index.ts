6f9c9dbce18d2a304b1faeeaa01a5bfa5be0fe01
Samael Medina <samaelortiz2218@gmail.com>
2026-08-06 08:52:05 -0700

    feat: crear index

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-import express
+import express, { type Express, type Request, type Response} from "express";
+import 'dotenv/config'
+
+const app: Express = express()
+
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Hello Word");
+});
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, ()=>{
+console.log(`Servidor corriendo en el puerto ${PORT}`)
+})
